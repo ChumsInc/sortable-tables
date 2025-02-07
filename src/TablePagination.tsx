8c380ccddace0e2550cd61d7a6cb63815a824ed3
Steve Montgomery <steve@chums.com>
2025-02-07 11:46:46 -0700

    chore: move aria tags on pagination buttons

--- src/TablePagination.tsx
+++ src/TablePagination.tsx
@@ -1,4 +1,4 @@
-import React, {useId} from 'react';
+import React from 'react';
 import RowsPerPage from "./RowsPerPage";
 import classNames from "classnames";
 import {TablePaginationProps} from "./types";
@@ -37,28 +37,28 @@ function TablePagination({
                     {showFirst && (
                         <div className="col-auto">
                             <button className={buttonClassName} disabled={page === 0}
-                                    onClick={() => onChangePage(0)}>
-                                <span className="bi-chevron-bar-left" aria-label="First page" />
+                                    onClick={() => onChangePage(0)} aria-label="First page">
+                                <span className="bi-chevron-bar-left" aria-hidden="true"/>
                             </button>
                         </div>
                     )}
                     <div className="col-auto">
                         <button className={buttonClassName} disabled={page === 0}
-                                onClick={() => onChangePage(page - 1)}>
-                            <span className="bi-chevron-left" aria-label="Previous page" />
+                                onClick={() => onChangePage(page - 1)} aria-label="Previous page">
+                            <span className="bi-chevron-left" aria-hidden="true"/>
                         </button>
                     </div>
                     <div className="col-auto">
                         <button className={buttonClassName} disabled={page >= lastPage}
-                                onClick={() => onChangePage(page + 1)}>
-                            <span className="bi-chevron-right" aria-label="Next page" />
+                                onClick={() => onChangePage(page + 1)} aria-label="Next page">
+                            <span className="bi-chevron-right" aria-hidden="true"/>
                         </button>
                     </div>
                     {showLast && (
                         <div className="col-auto">
                             <button className={buttonClassName} disabled={page >= lastPage}
-                                    onClick={() => onChangePage(lastPage)}>
-                                <span className="bi-chevron-bar-right" aria-label="Last page" />
+                                    onClick={() => onChangePage(lastPage)} aria-label="Last page">
+                                <span className="bi-chevron-bar-right" aria-hidden="true"/>
                             </button>
                         </div>
                     )}
