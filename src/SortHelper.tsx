8a5fa7dd172b71b6ca37f16e291421e48fcf73f2
Steve Montgomery <steve@chums.com>
2026-04-17 11:18:01 -0600

    chore: update npm dependencies and remove `console.log` in `SortHelper`

--- src/SortHelper.tsx
+++ src/SortHelper.tsx
@@ -8,7 +8,6 @@ export interface StandaloneSortHelperProps<T = unknown> {
 export function SortHelper<T = unknown>({nextSort}:StandaloneSortHelperProps<T>) {
     const [, setNextSort] = useTableSort<T>();
     useEffect(() => {
-        console.log('setNextSort', nextSort);
         setNextSort(nextSort);
     }, [nextSort, setNextSort]);
     return null;
